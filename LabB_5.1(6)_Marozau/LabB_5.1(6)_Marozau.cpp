56b27ba7440a785426fb9dda3cc5d42bbac9d8b7
marozauandrei77@gmail.com <marozauandrei77@gmail.com>
2024-05-20 10:21:44 +0300

    Добавьте файлы проекта.

---
+++ LabB_5.1(6)_Marozau/LabB_5.1(6)_Marozau.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+//6. Из заданного текста удалить те символы, которые встречаются в нем более
+//двух раз.
+
+//4.1
+void DeleteDoubleElements(string &str, int newSize) {
+    unsigned amountOfElements[256] = { 0 };
+
+    for (int i = 0; i < str.size(); i++) {
+        amountOfElements[int(str[i])]++;
+    }
+    int size = str.size();
+    for (int j = 0; j < size; j++) {
+        if (amountOfElements[int(str[j])] > 2) {
+            str.erase(j, 1);
+            size--;
+            j--;
+        }
+    }
+}
+
+//6. Написать программу перевода вещественных чисел из десятичной системы
+//счисления в шестнадцатеричную.
+//4.2
+void from10To16(string& str) {
+    string integerPart = ;
+    string fractionalPart = ;
+}
+
+//4.3
+
+
+
+
+int main()
+{
+    string P = "kaaaaaaaak";
+    DeleteDoubleElements(P, 15);
+    cout << P;
+
+}
+
